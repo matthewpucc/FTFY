3df3ac4aa321c8cd98961371bf0e7ef08538b055
Matthew Puccerella <matthewpucc@gmail.com>
2012-08-18 23:57:04 -0500

    Added grease / tamper monkey header

--- GoogleSearch/pwn.js
+++ GoogleSearch/pwn.js
@@ -1,15 +1,25 @@
+// ==UserScript==
+// @name       Fix google search links
+// @namespace  https://github.com/matthewpucc
+// @version    0.1
+// @description  Prevents google from hijacking clicks and converting it to google tracking. 
+//               This will disable the block this site ability.
+// @match      https://encrypted.google.com/search*
+// @copyright  2012+, Matthewpucc
+// ==/UserScript==
+
 (function() {
-	var lameness = document.querySelectorAll('h3.r');
-	for (var i = 0; i < lameness.length; i++) {
-		var a = lameness[i].childNodes[0];
+  var lameness = document.querySelectorAll('h3.r');
+  for (var i = 0; i < lameness.length; i++) {
+    var a = lameness[i].childNodes[0];
 
-		if (a && a.attributes && a.attributes.onmousedown) {
-			delete a.attributes.onmousedown;
-		}
+    if (a && a.attributes && a.attributes.onmousedown) {
+      delete a.attributes.onmousedown;
+    }
 
-		a.onmousedown = function (e) {
-			e.preventDefault();
-			e.stopPropagation();
-		};
-	}
+    a.onmousedown = function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+  }
 })();
