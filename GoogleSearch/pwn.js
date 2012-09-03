1395eac9c74a31f444b43596328c9f0444df178e
matthewpucc <matthewpucc@gmail.com>
2012-09-02 21:01:31 -0500

    More license and stuff

--- GoogleSearch/pwn.js
+++ GoogleSearch/pwn.js
@@ -1,13 +1,22 @@
 // ==UserScript==
 // @name          Fix google search links
 // @namespace     https://github.com/matthewpucc
-// @version       0.2
+// @version       0.3
 // @updateURL     https://raw.github.com/matthewpucc/FTFY/master/GoogleSearch/pwn.js
 // @description   Prevents google from hijacking clicks and converting it to google tracking. 
 //                This will disable the block this site ability.
 // @include       https://encrypted.google.com/search*
-// @copyright     2012+, Matthewpucc
+// @copyright     2012+, matthewpucc, Beerware
 // ==/UserScript==
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * matthewpucc wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return
+ * ----------------------------------------------------------------------------
+ */
 
 (function() {
   var lameness = document.querySelectorAll('h3.r');
--- NetflixFix/pwn.js
+++ NetflixFix/pwn.js
@@ -1,13 +1,23 @@
 // ==UserScript==
-// @name          Open Netflix Movies as more details
+// @name          Open Netflix Movies as Detail View
 // @namespace     https://github.com/matthewpucc
-// @version       0.1
+// @version       0.3
+// @updateURL     https://raw.github.com/matthewpucc/FTFY/master/NetflixFix/pwn.js
 // @description   This will rewrite the image links in the default netflix views to open the information page
 //                instead of forcing the movie/show to start playing. The makes managing a queue much easier 
 // @include       http://*netflix.com/search*
 // @exclude       http://movies.netflix.com/WiPlayer*
-// @copyright     2012+, Matthewpucc
+// @copyright     2012+, matthewpucc, Beerware
 // ==/UserScript==
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * matthewpucc wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return
+ * ----------------------------------------------------------------------------
+ */
 
 (function () {
   var aTags     = document.getElementsByTagName('a'),
